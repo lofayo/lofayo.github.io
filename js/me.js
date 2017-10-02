20555d1a6f38ae247c9b39e40f78ce0f7a965c71
lofayo <870876711@qq.com>
2017-10-02 09:25:59 +0800

    Site updated: 2017-10-02 09:25:43

--- js/me.js
+++ js/me.js
@@ -1,6 +1,15 @@
 $(function() {
+    // 文章预览时，每篇文章的标题
+
     $('.recent-post-item').each(function() {
         var title = $(this).find('.recent-post-link').attr('href').replace(/\//g, '');
+
         $(this).find('.post-excerpt').children().html(title);
     })
-})()
+
+    // 进入一篇文章详读时，文章标题
+
+    $('.article_title').each(function() {
+        $(this).html(title);
+    })
+})();
